4a44a9bcfedd9232f1c6fb784797be960adefaa2
Matias <117133660+matiasgraneros@users.noreply.github.com>
2025-01-07 21:56:09 +0000

    CORS modification

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 const corsOptions = {
-  origin: ['http://localhost:5173'],
+  origin: ['https://stock-smart.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
